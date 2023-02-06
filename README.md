628bc82d9a079dc139f31fddc9fbaf703ddab721
Md. Omar Faruk Anik <void.anikbd@gmail.com>
2023-02-06 13:38:21 +0600

    initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# flutter_architecture_demo
- Demo app to test the best architecture to make an flutter application.
--- lib/utils/routes/routes_name.dart
+++
@@ -1,5 +0,0 @@
-class RoutesName{
-  static const String login = "login_view";
-  static const String home = "home_view";
-  static const String splash = "splash_view";
-}
