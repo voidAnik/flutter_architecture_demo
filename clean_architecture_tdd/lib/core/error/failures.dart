74360dc93c1f3901edcbbb23759ff1d8bf50e055
Md. Omar Faruk Anik <void.anikbd@gmail.com>
2023-02-11 12:42:09 +0600

    Update failures.dart

--- clean_architecture_tdd/lib/core/error/failures.dart
+++ clean_architecture_tdd/lib/core/error/failures.dart
@@ -3,7 +3,7 @@ import 'package:equatable/equatable.dart';
 abstract class Failure extends Equatable {
   // If the subclasses have some properties, they'll get passed to this constructor
   // so that Equatable can perform value comparison.
-  const Failure({this.properties =  const <dynamic>[]});
+  const Failure({this.properties = const <dynamic>[]});
 
   final List properties;
 
@@ -11,20 +11,6 @@ abstract class Failure extends Equatable {
   List<Object> get props => [properties];
 }
 
-class ServerFailure extends Failure{}
+class ServerFailure extends Failure {}
 
-class CacheFailure extends Failure{}
-
-extension on Failure{
-
-  String getMsg(){
-    switch(runtimeType){
-      case ServerFailure:
-        return "1";
-      case CacheFailure:
-        return "2";
-      default:
-        return "Unexpected";
-    }
-  }
-}
+class CacheFailure extends Failure {}
